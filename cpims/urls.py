469fba5881dcc35bead47bd7eacfbe25e3e912b4
Newton Mugaya <nmugaya@gmail.com>
2015-11-24 14:43:24 +0300

    Fixed static and urls

--- cpims/urls.py
+++ cpims/urls.py
@@ -5,6 +5,8 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from cpovc_auth import urls as auth_urls
 from cpovc_registry import urls as registry_urls
+from cpovc_forms import urls as forms_urls
+from cpovc_reports import urls as reports_urls
 
 
 urlpatterns = [
@@ -14,7 +16,9 @@ urlpatterns = [
     url(r'^logout/$', 'cpovc_auth.views.log_out', name='logout'),
     url(r'^register/$', 'cpovc_auth.views.register', name='register'),
     url(r'^auth/$', include(auth_urls)),
-    url(r'^registry/', include(registry_urls))
+    url(r'^registry/', include(registry_urls)),
+    url(r'^forms/', include(forms_urls)),
+    url(r'^reports/', include(reports_urls))
 ]
 
 handler400 = 'cpims.views.handler_400'
