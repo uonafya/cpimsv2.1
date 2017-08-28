4f7909f4e15361658b4aa0da974c39d214e2aa75
Newton Mugaya <nmugaya@gmail.com>
2017-08-28 12:35:49 +0300

    Remove all permissions

--- cpovc_auth/decorators.py
+++ cpovc_auth/decorators.py
@@ -1,203 +1,67 @@
 """Decorator to handle permissions."""
-import re
 from functools import wraps
 from django.shortcuts import render, get_object_or_404
 from django.utils.decorators import available_attrs
-from django.core.urlresolvers import resolve
 from cpovc_registry.models import (
     RegPersonsAuditTrail, RegOrgUnitsAuditTrail, RegPersonsGeo,
     RegPersonsOrgUnits, RegOrgUnit, RegPerson)
-from cpovc_auth.models import AppUser
+from .perms import PERM
 
 ORG_GROUPS = ['DEC', 'DSU', 'DUU', 'RGU']
 
-ORGS_TEXT = {'registry/new/': 'Create new Organisational unit.',
-             'registry/edit/': 'Edit Organisational unit.',
-             'registry/view/': 'View Organisational unit details.',
-             'registry/new_person/': 'Register a new person.',
-             'registry/edit_person/': 'Edit existing person.',
-             'registry/view_person/': 'View person details.',
-             'auth/roles/': 'Manage roles',
-             'registry/new_user/': 'Create a new login account.'}
 
-COUNTY_TEXT = {}
-ALLOWED_PAGES = ['registry/new', 'registry/new_person', 'registry/new_user']
-ALLOWED_OWN = ['registry/edit', 'registry/edit_person']
-
-
-def is_allowed_ous(allowed_groups, page=1):
+def is_allowed_groups(allowed_groups, page=1):
     """Method for checking roles and permissions."""
     def decorator(check_func):
         @wraps(check_func, assigned=available_attrs(check_func))
         def _wrapped_view(request, *args, **kwargs):
             # If active super user lets just proceed
             print 'url', request.path_info
+            page_ids = str(page).zfill(2)
+            page_type = page_ids[1:]
+            print page_type
             if request.user.is_active and request.user.is_superuser:
                 return check_func(request, *args, **kwargs)
             else:
+                level_perms = PERM[page] if page in PERM else {}
+                print level_perms
+                is_ovc, is_dcs, is_nat = False, False, False
+                # cbo_id = request.session.get('ou_primary', 0)
+                reg_ovc = request.session.get('reg_ovc', 0)
+                reg_nat = request.session.get('is_national', 0)
+                ou_perms = request.session.get('ou_perms', 0)
+                ou_attached = request.session.get('ou_attached', 0)
+                user_level = request.session.get('user_level', 0)
+                print 'oup', ou_perms, ou_attached, user_level
+                if level_perms['ovc'] and reg_ovc:
+                    is_ovc = True
+                if level_perms['dcs'] and not reg_ovc:
+                    is_dcs = True
+                if level_perms['ho'] and reg_nat:
+                    is_nat = True
+                ovc_check = (is_ovc, is_dcs, is_nat)
                 from .functions import get_groups
                 grps = request.user.groups.values_list('id', flat=True)
+                print 'grps', grps
                 cpgrp = get_groups('')
                 cpims_grps = [cpgrp[grp] for grp in grps if grp in cpgrp]
+                print cpims_grps
                 gen_groups = [x for x in cpims_grps if x not in ORG_GROUPS]
-                response = any(value in gen_groups for value in allowed_groups)
+                print ovc_check, gen_groups, cpims_grps
+                response = any(value in cpims_grps for value in allowed_groups)
                 if response:
-                    return check_func(request, *args, **kwargs)
-                current_url = resolve(request.path_info).url_name
-                pg_exists = current_url in ORGS_TEXT
-                page_info = ORGS_TEXT[current_url] if pg_exists else 'details'
+                    if any(ovc_check):
+                        return check_func(request, *args, **kwargs)
+                # current_url = resolve(request.path_info).url_name
+                page_info = 'Permission denied'
                 return render(request, 'registry/roles_none.html',
                               {'page': page_info})
         return _wrapped_view
     return decorator
 
 
-def is_allowed_groups(allowed_groups):
-    """Method for checking roles and permissions."""
-    def decorator(check_func):
-        @wraps(check_func, assigned=available_attrs(check_func))
-        def _wrapped_view(request, *args, **kwargs):
-            # If active super user lets just proceed
-            if request.user.is_active and request.user.is_superuser:
-                return check_func(request, *args, **kwargs)
-            from .functions import get_groups, get_allowed_units_county
-            # None restrictive roles
-            grps = request.user.groups.values_list('id', flat=True)
-            cpims_grp = get_groups('')
-            # Get org units and sub-counties
-            user_id = request.user.id
-            ex_areas, ex_orgs = get_allowed_units_county(user_id)
-            ogrps, pgrps = allowed_org_county(ex_areas, ex_orgs, request)
-            print ogrps, pgrps, grps, ex_orgs
-            # Do the check now for non-restrictive groups
-            cpims_groups = [cpims_grp[grp] for grp in grps if grp in cpims_grp]
-            gen_groups = [x for x in cpims_groups if x not in ORG_GROUPS]
-            response = any(value in gen_groups for value in allowed_groups)
-            # Do the check now for org unit and sub-county
-            org_check, allow_create = False, False
-            if ogrps:
-                ogroups = [cpims_grp[grp] for grp in ogrps if grp in cpims_grp]
-                org_check = any(value in ogroups for value in allowed_groups)
-            # These urls
-            orgp_grps = [3, 7]
-            org_person = any(value in orgp_grps for value in grps)
-            cur_url = request.path_info.strip("/")
-            # cur_url = re.sub('\d', '', curr_url)
-            if org_person and cur_url in ALLOWED_PAGES:
-                allow_create = True
-            print 'ROLES', response, org_check
-            if response or org_check or allow_create or pgrps:
-                return check_func(request, *args, **kwargs)
-            else:
-                from django.core.urlresolvers import resolve
-                current_url = resolve(request.path_info).url_name
-                pg_exists = current_url in ORGS_TEXT
-                page_info = ORGS_TEXT[current_url] if pg_exists else 'details'
-                return render(request, 'registry/roles_none.html',
-                              {'page': page_info})
-        return _wrapped_view
-    return decorator
-
-
-def allowed_org_county(areas, orgs, request):
-    """
-    Method to try decipher url and determine id being requested.
-
-    Then also check if it is an org unit or its an area
-    """
-    try:
-        org_url = None
-        cur_url = request.path_info
-        group_orgs, all_persons = None, None
-        person_id, item_id = 0, 0
-        plain_url = cur_url.lstrip("/")
-        for org_text in ORGS_TEXT:
-            if plain_url.startswith(org_text):
-                org_url = org_text
-        if org_url:
-            print 'CUR URL', org_url
-            match_org = re.match(r"^(.?)/%s(\d+)(.?)$" % (org_url), cur_url)
-            if match_org:
-                item_id = int(match_org.group(2))
-                if 'person' in cur_url or 'user' in cur_url:
-                    person_id = item_id
-                if 'auth' in cur_url:
-                    person_id = get_user_ids(request, item_id)
-                else:
-                    if item_id in orgs:
-                        group_orgs = orgs[item_id]
-            all_persons = get_person_ids(request, person_id, item_id)
-        return group_orgs, all_persons
-    except Exception, e:
-        error = "Error in getting allowed org units / county - %s" % (str(e))
-        print error
-        return False
-
-
-def get_user_ids(request, user_id):
-    """Method to get person id from user id."""
-    try:
-        person_obj = AppUser.objects.get(id=user_id)
-        person_id = person_obj.reg_person_id
-        return person_id
-    except Exception:
-        pass
-
-
-def get_person_ids(request, person_id, item_id):
-    """Method to get person ids in area and org unit of jurisdiction."""
-    try:
-        # user_id = request.user.id
-        # cur_url = request.path_info.strip("/")
-        cur_person_id = request.user.reg_person_id
-        cur_user_id = request.user.id
-        if person_id == cur_person_id:
-            return [person_id]
-        # Determine who created this record from Persons audit trail
-        if person_id and person_id == item_id:
-            print 'Here in Persons'
-            record_details = get_creator_details(person_id)
-            if record_details:
-                creator_id = record_details.created_by.pk
-                creator_pid = record_details.created_by.reg_person_id
-                print 'person ids', creator_id, person_id
-                if creator_id == cur_user_id:
-                    return [creator_id]
-                # Determine if these two work in the same org unit
-                # For Government, NGO and Volunteer
-                org_units = check_workmate(creator_pid, cur_person_id)
-                if org_units:
-                    return org_units
-            else:
-                # If Child / Caregiver check if its same area of jurisdiction
-                print 'This is a fellow staff member.'
-                # I did not create but we work in same organisation
-                check_wm = check_workmate(person_id, cur_person_id)
-                return check_wm
-
-        else:
-            # Determine who created this record from Org Units audit trail
-            if item_id and not person_id:
-                print 'Here in Org Units'
-                record_details = get_audit_details(item_id, 'Unit')
-                if record_details:
-                    creator_id = record_details.app_user_id
-                    if creator_id == cur_user_id:
-                        return [creator_id]
-                    # Determine if they work in the same org unit.
-                    org_units = check_workmate(creator_pid, cur_person_id, 'U')
-                    if org_units:
-                        return org_units
-            else:
-                print 'Here in Roles management'
-                check_wm = check_workmate(person_id, cur_person_id)
-                return check_wm
-        return []
-    except Exception, e:
-        print 'Error - %s' % (str(e))
-        pass
-    else:
-        return []
+def is_allowed_ous(allowed_groups, page=1):
+    return is_allowed_groups(allowed_groups, page)
 
 
 def get_creator_details(item_id, audit_type='Person'):
---
+++ cpovc_auth/perms.py
@@ -0,0 +1,56 @@
+PERM = {}
+# ovc, dcs, head office
+# Org units
+PERM[10] = {'ovc': 1, 'dcs': 1, 'ho': 1, 'perms': []}
+PERM[11] = {'ovc': 1, 'dcs': 1, 'ho': 1, 'perms': []}
+PERM[12] = {'ovc': 1, 'dcs': 1, 'ho': 1, 'perms': []}
+PERM[13] = {'ovc': 1, 'dcs': 1, 'ho': 1, 'perms': []}
+PERM[14] = {'ovc': 1, 'dcs': 1, 'ho': 1, 'perms': []}
+# Persons
+PERM[20] = {'ovc': 1, 'dcs': 1, 'ho': 1, 'perms': []}
+PERM[21] = {'ovc': 1, 'dcs': 1, 'ho': 1, 'perms': []}
+PERM[22] = {'ovc': 1, 'dcs': 1, 'ho': 1, 'perms': []}
+PERM[23] = {'ovc': 1, 'dcs': 1, 'ho': 1, 'perms': []}
+PERM[24] = {'ovc': 1, 'dcs': 1, 'ho': 1, 'perms': []}
+# Accounts and Roles
+PERM[30] = {'ovc': 1, 'dcs': 1, 'ho': 1, 'perms': []}
+PERM[31] = {'ovc': 1, 'dcs': 1, 'ho': 1, 'perms': []}
+PERM[32] = {'ovc': 1, 'dcs': 1, 'ho': 1, 'perms': []}
+PERM[33] = {'ovc': 1, 'dcs': 1, 'ho': 1, 'perms': []}
+PERM[34] = {'ovc': 1, 'dcs': 1, 'ho': 1, 'perms': []}
+# OVC Care
+PERM[40] = {'ovc': 1, 'dcs': 0, 'ho': 1, 'perms': []}
+PERM[41] = {'ovc': 1, 'dcs': 0, 'ho': 1, 'perms': []}
+PERM[42] = {'ovc': 1, 'dcs': 0, 'ho': 0, 'perms': []}
+PERM[43] = {'ovc': 1, 'dcs': 0, 'ho': 0, 'perms': []}
+PERM[44] = {'ovc': 1, 'dcs': 0, 'ho': 0, 'perms': []}
+# Forms - Case record
+PERM[50] = {'ovc': 1, 'dcs': 1, 'ho': 1, 'perms': []}
+PERM[51] = {'ovc': 1, 'dcs': 1, 'ho': 1, 'perms': []}
+PERM[52] = {'ovc': 1, 'dcs': 1, 'ho': 1, 'perms': []}
+PERM[53] = {'ovc': 1, 'dcs': 1, 'ho': 1, 'perms': []}
+PERM[54] = {'ovc': 1, 'dcs': 1, 'ho': 1, 'perms': []}
+# Others
+PERM[60] = {'ovc': 1, 'dcs': 1, 'ho': 1, 'perms': []}
+PERM[61] = {'ovc': 1, 'dcs': 1, 'ho': 1, 'perms': []}
+PERM[62] = {'ovc': 1, 'dcs': 1, 'ho': 1, 'perms': []}
+PERM[63] = {'ovc': 1, 'dcs': 1, 'ho': 1, 'perms': []}
+PERM[64] = {'ovc': 1, 'dcs': 1, 'ho': 1, 'perms': []}
+#
+PERM[70] = {'ovc': 1, 'dcs': 1, 'ho': 1, 'perms': []}
+PERM[71] = {'ovc': 1, 'dcs': 1, 'ho': 1, 'perms': []}
+PERM[72] = {'ovc': 1, 'dcs': 1, 'ho': 1, 'perms': []}
+PERM[73] = {'ovc': 1, 'dcs': 1, 'ho': 1, 'perms': []}
+PERM[74] = {'ovc': 1, 'dcs': 1, 'ho': 1, 'perms': []}
+#
+PERM[80] = {'ovc': 1, 'dcs': 1, 'ho': 1, 'perms': []}
+PERM[81] = {'ovc': 1, 'dcs': 1, 'ho': 1, 'perms': []}
+PERM[82] = {'ovc': 1, 'dcs': 1, 'ho': 1, 'perms': []}
+PERM[83] = {'ovc': 1, 'dcs': 1, 'ho': 1, 'perms': []}
+PERM[84] = {'ovc': 1, 'dcs': 1, 'ho': 1, 'perms': []}
+#
+PERM[90] = {'ovc': 1, 'dcs': 1, 'ho': 1, 'perms': []}
+PERM[91] = {'ovc': 1, 'dcs': 1, 'ho': 1, 'perms': []}
+PERM[92] = {'ovc': 1, 'dcs': 1, 'ho': 1, 'perms': []}
+PERM[93] = {'ovc': 1, 'dcs': 1, 'ho': 1, 'perms': []}
+PERM[94] = {'ovc': 1, 'dcs': 1, 'ho': 1, 'perms': []}
